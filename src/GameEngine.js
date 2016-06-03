e29d650e5338eb6e3d82dd645b837c5e450fe8ad
designspin <admin@designspin.co.uk>
2016-06-03 20:27:13 +0100

    Initial commit

---
+++ src/GameEngine.js
@@ -0,0 +1,118 @@
+import Timer from './GameTimer';
+
+default export class {
+	constructor() {
+		this.entities = [];
+		this.ctx = null;
+		this.click = null;
+		this.timer = new Timer();
+		this.surfaceWidth = null;
+		this.surfaceHeight = null;
+		this.scale = null;
+	}
+
+	init(ctx) {
+		this.ctx = ctx;
+		this.surfaceWidth = this.ctx.canvas.width;
+		this.surfaceHeight = this.ctx.canvas.height;
+		this.setSurface();
+		this.startInput();
+	}
+
+	start() {
+	 let gameLoop = (() => {
+	 	this.loop();
+	 	requestAnimationFrame(gameLoop, this.ctx.canvas);
+	 })();
+	}
+
+	setSurface() {
+		let reflowStage = () => {
+			const scaleX = window.innerWidth / ctx.canvas.width;
+			const scaleY = window.innerHeight / ctx.canvas.height;
+			let scale = Math.min(scaleX, scaleY);
+			this.scale = scale;
+			ctx.canvas.parentNode.webkitTransform = "scale(" + scale + ")";
+			ctx.canvas.parentNode.transform = "scale(" + scale + ")";
+		}
+
+		window.addEventListener('resize', () => {
+			reflowStage();
+		});
+
+		window.addEventListener('orientationchage', () => {
+			reflowStage();
+		})
+	}
+
+	startInput() {
+		let getXandY = (e) => {
+			let x = e.clientX - this.ctx.canvas.getBoundingClientRect().left;
+			let y = e.clientY - this.ctx.canvas.getBoundingClientRect().top;
+
+			return {x: x, y: y};
+		}
+
+		let getTouchXandTouchY = (e) => {
+			let x = e.targetTouches[0].pageX - this.ctx.canvas.clientLeft;
+			let y = e.targetTouches[0].pageY - this.ctx.canvas.clientTop;
+
+			return {x: x, y: y};
+		}
+
+		this.ctx.canvas.addEventListener('click', (e) => {
+			this.click = getXandY(e);
+		}, false);
+
+		this.ctx.canvas.addEventListener('touchstart', (e) => {
+			e.preventDefault();
+			this.click = getTouchXandTouchY(e);
+		});
+	}
+
+	loop() {
+		this.clockTick = this.timer.tick();
+		this.update();
+		this.draw();
+		this.click = null;
+	}
+
+	addEntity(entity) {
+		this.entities.push(entity);
+	}
+
+	draw(drawCallback) {
+		this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+		this.ctx.save();
+		this.ctx.translate(0, this.ctx.canvas.height);
+		this.ctx.scale(1, -1);
+
+		for(let i = 0; i < this.entites.length; i++) {
+			this.entities[i].draw(this.ctx);
+		}
+
+		if(drawCallback) {
+			drawCallback(this);
+		}
+
+		this.ctx.restore();
+	}
+
+	update() {
+		let entitiesCount = this.entities.length;
+
+		for(let i = 0; i < entitiesCount; i++) {
+			let entity = this.entities[i];
+
+			if(!entity.removeFromWorld) {
+				entity.update();
+			}
+		}
+
+		for(let i = this.entities.length-1; i >= 0; --i) {
+			if(this.entities[i].removeFromWorld) {
+				this.entities.splice(i, 1);
+			}
+		}
+	}
+}
---
+++ src/GameTimer.js
@@ -0,0 +1,21 @@
+export default class {
+	constructor() {
+		this.gameTime = 0;
+		this.max = 0.05;
+		this.wallLastTimestamp = 0;
+		this.fps = 0;
+	}
+
+	tick() {
+		const wallCurrent = Date.now();
+		let wallDelta = (wallCurrent - this.wallLastTimestamp) / 1000;
+
+		this.fps = 1000 / (wallCurrent - this.wallLastTimestamp);
+		this.wallLastTimestamp = wallCurrent;
+		var gameDelta = Math.min(wallDelta, this.maxStep);
+
+		this.gameTime += gameDelta;
+
+		return gameDelta;
+	}
+}
